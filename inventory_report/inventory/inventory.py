a6c5b56ee54de03efe786aa01bae62a4b178e673
Philip Lages <psilvaelages@gmail.com>
2023-04-08 17:24:54 -0300

    fix: Fixes import statements

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,14 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-from inventory_report.importer import CsvImporter
-from inventory_report.importer import JsonImporter
-from inventory_report.importer import XmlImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
     @staticmethod
     def import_data(path: str, report_type: str) -> str:
-        data = []
+        data = list
 
         if path.endswith(".csv"):
             data = CsvImporter.import_data(path)
