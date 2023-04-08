fac7cbf7a1fdf82a3a367175ae8e468cb6d43e77
Philip Lages <psilvaelages@gmail.com>
2023-04-08 17:35:31 -0300

    feat: Creates test for relatorio_produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "ChocoBoom",
+        "Doces Trint",
+        "2023-04-08",
+        "2023-12-08",
+        "1234567890",
+        "Em local refrigerado",
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "ChocoBoom"
+    assert product.nome_da_empresa == "Doces Trint"
+    assert product.data_de_fabricacao == "2023-04-08"
+    assert product.data_de_validade == "2023-12-08"
+    assert product.numero_de_serie == "1234567890"
+    assert product.instrucoes_de_armazenamento == "Em local refrigerado"
