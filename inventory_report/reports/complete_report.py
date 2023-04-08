70ea8960bb939cd878f66266e945279f45775499
Philip Lages <psilvaelages@gmail.com>
2023-04-08 11:17:24 -0300

    refactor: Refactors CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -6,14 +6,13 @@ class CompleteReport(SimpleReport):
     @classmethod
     def generate(cls, companies_data: List[dict]) -> str:
         simple_report = SimpleReport.generate(companies_data)
+        products_by_company = cls.get_products_by_company(companies_data)
 
         complete_report = (
             f"{simple_report}\n" "Produtos estocados por empresa:\n"
         )
 
-        for company, count in cls.get_products_by_company(
-            companies_data
-        ).items():
+        for company, count in products_by_company.items():
             complete_report += f"- {company}: {count}\n"
 
         return complete_report
